050665fe436c3c852947b972c8a1684e5b24bde4
om omar <124066052+nedalalshami@users.noreply.github.com>
2024-05-27 21:29:15 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,112 @@
+let divselect =document.getElementById("div-select");
+function getimg(){
+    divselect.classList.add("active")
+   };
+let div2select =document.getElementById("div2-select");
+
+    function getimg2(){
+        div2select.classList.add("active") 
+};
+let div3select =document.getElementById("div3-select");
+function getimg3(){
+    div3select.classList.add("active") 
+}
+let div4select =document.getElementById("div4-select");
+function getimg4(){
+    div4select.classList.add("active") ; 
+}
+let div5select =document.getElementById("div5-select");
+function getimg5(){
+    div5select.classList.add("active") ; 
+}
+let div6select =document.getElementById("div6-select");
+
+function getimg6(){
+    div6select.classList.add("active") ;
+}
+let div7select =document.getElementById("div7-select");
+
+function getimg7(){
+    div7select.classList.add("active") ;
+
+};
+let div8select =document.getElementById("div8-select");
+
+function getimg8(){
+    div8select.classList.add("active") ;
+
+};
+let div9select =document.getElementById("div9-select");
+
+function getimg9(){
+    div9select.classList.add("active") ;
+
+};
+let div10select =document.getElementById("div10-select");
+
+function getimg10(){
+    div10select.classList.add("active") ;
+
+};
+let div11select =document.getElementById("div11-select");
+
+function getimg11(){
+    div11select.classList.add("active") ;
+
+};
+
+let div12select =document.getElementById("div12-select");
+
+function getimg12(){
+    div12select.classList.add("active") ;
+
+};
+let div13select =document.getElementById("div13-select");
+
+function getimg13(){
+    div13select.classList.add("active") ;
+
+};
+let div14select =document.getElementById("div14-select");
+
+function getimg14(){
+    div14select.classList.add("active") ;
+
+};
+let div15select =document.getElementById("div15-select");
+
+function getimg15(){
+    div15select.classList.add("active") ;
+
+};
+let div16select =document.getElementById("div16-select");
+
+function getimg16(){
+    div16select.classList.add("active") ;
+
+};
+let div17select =document.getElementById("div17-select");
+
+ function getimg17(){
+    div17select.classList.add("active") ;
+
+};
+let div18select =document.getElementById("div18-select");
+
+function getimg18(){
+    div18select.classList.add("active") ;
+
+};
+let div19select =document.getElementById("div19-select");
+
+function getimg19(){
+    div19select.classList.add("active") ;
+
+};
+let div20select =document.getElementById("div20-select");
+
+function getimg20(){
+    div20select.classList.add("active") ;
+
+};
+
